fe6b07c92eb7afd7fe93d4b2b13ed2614f5dbc0a
jackup <yk.58@hotmail.com>
2021-12-01 16:12:38 +0300

    Add project files.

---
+++ MCD_PersonelUygulamasi/Helper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_PersonelUygulamasi
+{
+    public static class Helper
+    {
+        //public void Test() { } Static class içinde static olmayan metod oluşturulamaz.
+        //public string Tanim { get; set; } Static class içinde static olmayan field - property tanımlamaları yapılamaz.
+
+        static Helper()
+        {
+            //Uygulama içerisinde ne kadar çağırılırsa çağırılsın sadece ilk çağrılmada çalışırlar.
+            Console.WriteLine("Helper => Static Class => Static CTOR");
+        }
+
+
+
+        public static void EmailGonder(string aliciEmailAdres, string konu, string icerik)
+        {
+            //Mail gönderme işlemleri devam edecek..
+            Console.WriteLine("Mail gönderim işlemi başarılı");
+        }
+    }
+}
---
+++ MCD_PersonelUygulamasi/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_PersonelUygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Helper g = new Helper(); static class'tan instance oluşturulamaz.
+        }
+    }
+}
