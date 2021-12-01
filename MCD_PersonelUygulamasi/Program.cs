70dadd580c074d2061c6a4d2f28442ff4f23a0f6
jackup <yk.58@hotmail.com>
2021-12-01 17:12:43 +0300

    code changed

--- MCD_PersonelUygulamasi/Program.cs
+++ MCD_PersonelUygulamasi/Program.cs
@@ -11,6 +11,22 @@ namespace MCD_PersonelUygulamasi
         static void Main(string[] args)
         {
             //Helper g = new Helper(); static class'tan instance oluşturulamaz.
+            Personel p1 = new Personel();
+            p1.Isim = "Yakup";
+            p1.Soyisim = "Kömük";
+            p1.EmailAdres = "yakup.komuk";
+            p1.Cinsiyet = 717770001; //-->Bay, 717770002 -->Bayan
+
+            Personel p2 = new Personel();
+            p1.Isim = "Ali";
+            p1.Soyisim = "Gel";
+            p1.EmailAdres = "ali.gel";
+            p1.Cinsiyet = 717770001;
+
+            Helper.EmailGonder("ik.firmam.com", "Yeni Personel Bilgilendirme", p1.Isim + p1.Soyisim);
+            Helper.EmailGonder("ik.firmam.com", "Yeni Personel Bilgilendirme", p2.Isim + p2.Soyisim);
+
+
         }
     }
 }
